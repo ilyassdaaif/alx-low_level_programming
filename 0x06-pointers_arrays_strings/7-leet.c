4a838ec4fea703e38c547d84b68935611dde496b
ilyassdaaif <ilyass.df.01@gmail.com>
2023-06-29 16:01:16 -0700

    a

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,23 @@
 
 char *leet(char *c)
 {
-	char *cp = c;
-	char Key[] = {'A', 'E', 'O', 'T', 'L'};
-	int value[] = {4, 3, 0, 7, 1};
-	unsigned int i;
+        char *cp = c;
+        char Key[] = {'A', 'E', 'O', 'T', 'L'};
+        int value[] = {4, 3, 0, 7, 1};
+        unsigned int i;
 
-	while (*c)
-	{
-		for (i = 0; i < sizeof(key) / sizeof(char); i++)
-		{
-			/*32 is the difference between lower case letters and apper case letters*/
-			if (*c == key[i] || *c == key[i] + 32)
-			{
-				*c = 48 + value[i];
-			}
-		}
-		c++;
-	}
+        while (*c)
+        {
+                for (i = 0; i < sizeof(key) / sizeof(char); i++)
+                {
+                        /*32 is the difference between lower case letters and apper case letters*/
+                        if (*c == key[i] || *c == key[i] + 32)
+                        {
+                                *c = 48 + value[i];
+                        }
+                }
+                c++;
+        }
 
-	return (cp);
+        return (cp);
 }
