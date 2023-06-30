1217b15683746fb5dedbfd0201511ac3b10c9e8b
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2023-06-30 02:41:48 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,21 +10,21 @@ char *leet(char *c)
 {
         char *cp = c;
         char Key[] = {'A', 'E', 'O', 'T', 'L'};
-        int value[] = {4, 3, 0, 7, 1};
+        int Value[] = {4, 3, 0, 7, 1};
         unsigned int i;
 
         while (*c)
         {
-                for (i = 0; i < sizeof(key) / sizeof(char); i++)
+                for (i = 0; i < sizeof(Key) / sizeof(char); i++)
                 {
-                        /*32 is the difference between lower case letters and apper case letters*/
-                        if (*c == key[i] || *c == key[i] + 32)
+                        /* 32 is the difference between lowercase letters and uppercase letters */
+                        if (*c == Key[i] || *c == Key[i] + 32)
                         {
-                                *c = 48 + value[i];
+                                *c = '0' + Value[i];
                         }
                 }
                 c++;
         }
 
-        return (cp);
+        return cp;
 }
