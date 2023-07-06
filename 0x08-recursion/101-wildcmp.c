cc9a57f01fff4f6d2d2e454d253757203840da3e
ilyassdaaif <ilyass.df.01@gmail.com>
2023-07-06 11:53:36 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,65 +1,31 @@
 #include "main.h"
 
 /**
- * move_past_star - iterates past asterisk
- * @s2: the second string, can contain wildcard
- *
- * Return: the pointer past star
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-char *move_past_star(char *s2)
-{
-if (*s2 == '*')
-return (move_past_star(s2 + 1));
-else
-return (s2);
-}
 
-#include "main.h"
-
-/**
- * move_past_star - iterates past asterisk
- * @s2: the second string, can contain wildcard
- *
- * Return: the pointer past star
- */
-char *move_past_star(char *s2)
-{
-if (*s2 == '*')
-return (move_past_star(s2 + 1));
-else
-return (s2);
-}
-
-/**
- * wildcmp - compares two strings lexicographically
- * @s1: the first string
- * @s2: the second string, can contain wildcard
- *
- * Return: 1 if identical, 0 if false
- */
 int wildcmp(char *s1, char *s2)
 {
-int ret = 0;
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
-if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-return (1);
-if (*s1 == *s2)
-{
-if (!*s1)
-return (1);
-return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
-if (!*s1 || !s2)
-return (0);
-if (*s2 == '*')
-{
-s2 = move_past_star(s2);
-if (!*s2)
-return (1);
-if (*s1 == *s2)
-ret += wildcmp(s1 + 1, s2 + 1);
-ret += inception(s1, s2);
-return (!!ret);
-}
-return (0);
-}
+
