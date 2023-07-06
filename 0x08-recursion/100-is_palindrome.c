538a40abfae75eaa336f50b6ae533a7f37843930
ilyassdaaif <ilyass.df.01@gmail.com>
2023-07-05 19:47:50 -0700

    a

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,11 +2,18 @@
 
 /**
  * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
+ * @s: pointer to the string
  * Return: int
  */
+int last_index(char *s);
 int is_palindrome(char *s);
 int check(char *s, int start, int end, int mod);
+
+/**
+ * last_index - returns the last index of a string (counts the null char)
+ * @s: pointer to the string
+ * Return: int
+ */
 int last_index(char *s)
 {
 int n = 0;
@@ -22,7 +29,6 @@ return (n);
  * @s: string to check
  * Return: 0 or 1
  */
-
 int is_palindrome(char *s)
 {
 int end = last_index(s);
@@ -38,16 +44,13 @@ return (check(s, 0, end - 1, end % 2));
  * @mod: int
  * Return: 0 or 1
  */
-
-
-
 int check(char *s, int start, int end, int mod)
 {
-
 if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
 return (1);
 else if (s[start] != s[end])
 return (0);
 else
-reutrn (check(s, start + 1, end - 1, mod));
+return (check(s, start + 1, end - 1, mod));
 }
+
