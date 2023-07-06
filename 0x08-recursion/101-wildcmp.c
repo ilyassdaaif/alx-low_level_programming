79ba55f30f1bfbd01b7e8b4fecb25388e8ee32ea
ilyassdaaif <ilyass.df.01@gmail.com>
2023-07-05 22:07:32 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,29 +8,26 @@
  */
 char *move_past_star(char *s2)
 {
-	if (*s2 == '*')
-		return (move_past_star(s2 + 1));
-	else
-		return (s2);
+if (*s2 == '*')
+return (move_past_star(s2 + 1));
+else
+return (s2);
 }
 
+#include "main.h"
+
 /**
- * inception - makes magic a reality
- * @s1: the first string
+ * move_past_star - iterates past asterisk
  * @s2: the second string, can contain wildcard
  *
- * Return: 1 if identical, 0 if false
+ * Return: the pointer past star
  */
-int inception(char *s1, char *s2)
+char *move_past_star(char *s2)
 {
-	int ret = 0;
-
-	if (*s1 == 0)
-		return (0);
-	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
-	return (ret);
+if (*s2 == '*')
+return (move_past_star(s2 + 1));
+else
+return (s2);
 }
 
 /**
@@ -42,27 +39,27 @@ int inception(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	int ret = 0;
+int ret = 0;
 
-	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
-		return (1);
-	if (*s1 == *s2)
-	{
-		if (!*s1)
-			return (1);
-		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
-	}
-	if (!*s1 || !s2)
-		return (0);
-	if (*s2 == '*')
-	{
-		s2 = move_past_star(s2);
-		if (!*s2)
-			return (1);
-		if (*s1 == *s2)
-			ret += wildcmp(s1 + 1, s2 + 1);
-		ret += inception(s1, s2);
-		return (!!ret);
-	}
-	return (0);
+if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+return (1);
+if (*s1 == *s2)
+{
+if (!*s1)
+return (1);
+return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+}
+if (!*s1 || !s2)
+return (0);
+if (*s2 == '*')
+{
+s2 = move_past_star(s2);
+if (!*s2)
+return (1);
+if (*s1 == *s2)
+ret += wildcmp(s1 + 1, s2 + 1);
+ret += inception(s1, s2);
+return (!!ret);
+}
+return (0);
 }
