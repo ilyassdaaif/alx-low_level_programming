826043f36dd1b81f35a8ceceff1b865d56764454
ilyassdaaif <ilyass.df.01@gmail.com>
2023-07-08 15:20:44 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *main-prints the minimum number of coins
- *to make change for an amount of money.
- *@argc:number of commandline arguments.
- *@argv:pointer to an array of command line arguments.
- *Return:0-success, non-zero-fail.
- */
 
+/**
+ * main - prints the minimum number of coins
+ * to make change for an amount of money.
+ * @argc: number of command line arguments.
+ * @argv: pointer to an array of command line arguments.
+ * Return: 0-success, non-zero-fail.
+ */
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-	int i, leastcents = 0, money = atoi(argv[1]);
-	int cents[] = {25, 10, 5, 2, 1};
+    if (argc == 2)
+    {
+        int i, coinCount = 0, money = atoi(argv[1]);
+        int cents[] = {25, 10, 5, 2, 1};
+        int numCoins = sizeof(cents) / sizeof(cents[0]);
 
-	for (i = 0; i < 5; i++)
-	{
-		if (money >= cents[i])
-		{
-			leastcents += money / cents[i];
-			money = money % cents[i];
-			if (money % cents[i] == 0)
-			{
-				break;
-			}
-		}
-	}
-	printf("%d\n", leastcents);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	return (0);
-} 
+        if (money == 0)
+        {
+            printf("0\n");
+            return 0;
+        }
+
+        for (i = 0; i < numCoins; i++)
+        {
+            if (money >= cents[i])
+            {
+                coinCount += money / cents[i];
+                money = money % cents[i];
+                if (money % cents[i] == 0)
+                {
+                    break;
+                }
+            }
+        }
+        printf("%d\n", coinCount);
+    }
+    else
+    {
+        printf("Error\n");
+        return 1;
+    }
+    return 0;
+}
