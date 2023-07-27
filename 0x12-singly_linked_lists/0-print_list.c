9999ee5bfd43f8c588288dccd984f84793b00ebc
ilyass <ilyass.df@gmail.com>
2023-07-27 11:50:17 -0700

    Finished

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,13 @@
  */
 int _strlen(char *s)
 {
-        int i = 0;
+int i = 0;
 
-        if (!s)
-                return (0);
-        while (*s++)
-                i++;
-        return (i);
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
 }
 
 /**
@@ -25,13 +25,13 @@ int _strlen(char *s)
  */
 size_t print_list(const list_t *h)
 {
-        size_t i = 0;
+size_t i = 0;
 
-        while (h)
-        {
-                printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-                h = h->next;
-                i++;
-        }
-        return (i);
+while (h)
+{
+printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+h = h->next;
+i++;
+}
+return (i);
 }
