924c59cc12af72ab33117a793c61690152b77992
ilyass <ilyass.df@gmail.com>
2023-07-31 11:05:35 -0700

    a

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
  * add_nodeint - adds a node to the top
  * @head: pointer to first node
  * @n: value for new node
- * 
+ *
  * Return: pointer to new node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
