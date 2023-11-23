cc1e20ceccb86cea695d8a70047298c529cfbd7a
ilyass <ilyass@gmail.com>
2023-11-23 10:01:47 -0800

    b

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: the bit state or -1 on error
  */
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
