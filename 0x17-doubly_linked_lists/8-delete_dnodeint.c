ecec515c437e518b8dc93948a29e71283819c514
ilyass <ilyass.df.01@gmail.com>
2024-01-11 12:55:48 -0800

    dll

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * delete_dnodeint_at_index - Deletes a node from a dlistint_t
- * 			      at a given index.
+ * at a given index.
  * @head: A pointer to the head of the dlistint_t.
  * @index: The index of the node to delete.
  *
  * Return: Upon success - 1.
- * 	   Otherwise - -1.
+ * Otherwise - -1.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (*head != NULL)
 			(*head)->prev = NULL;
 	}
-	
+
 	else
 	{
 		tmp->prev->next = tmp->next;
