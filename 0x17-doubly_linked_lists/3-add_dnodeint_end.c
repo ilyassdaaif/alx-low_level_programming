be0f4d8996516cd85ade9dc771c8f02a1215b79a
ilyass <ilyass.df.01@gmail.com>
2024-01-11 12:51:23 -0800

    dll

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,18 +9,18 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
+dlistint_t *new = malloc(sizeof(dlistint_t)), *node;
 
-        if (!head || !new)
-                return (new ? free(new), NULL : NULL);
+if (!head || !new)
+return (new ? free(new), NULL : NULL);
 
-        new->n = n;
-        new->next = NULL;
-        if (!*head)
-        {
-                new->prev = NULL;
+new->n = n;
+new->next = NULL;
+if (!*head)
+{
+new->prev = NULL;
 		*head = new;
-        }
+}
 	else
 	{
 		node = *head;
