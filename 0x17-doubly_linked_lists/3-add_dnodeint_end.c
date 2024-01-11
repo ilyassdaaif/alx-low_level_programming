f9105d71b9a4e9b8d7632d45c8096dce6f26412c
ilyass <ilyass.df.01@gmail.com>
2024-01-11 13:14:52 -0800

    dll

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds new head node to dlist
+ * add_dnodeint_end - adds node at the end to dlist
  * @head: address of pointer to current head node
  * @n: int field of new node
- *
+ * 
  * Return: address of new node or NULL
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
