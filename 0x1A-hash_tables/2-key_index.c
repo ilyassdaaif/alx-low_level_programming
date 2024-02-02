f3882b85d807d7f5cc1c4f8a8c8af94f777b0faf
ilyass <ilyass.df.01@gmail.com>
2024-02-02 12:22:51 -0800

    hash

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives the index of a key
- * @key: key to get index for
- * @size: size of the hash table
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size of hash table
  *
- * Return: index for the key
+ * Return: index of 'key' using djb2
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
