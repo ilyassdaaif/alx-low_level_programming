8d528f0a3a66782fc83d8f3c292c1a52a049636c
ilyass <ilyass@gmail.com>
2024-05-08 12:48:46 -0700

    0x1E search algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,7 +8,7 @@
  * @value: The value to search for.
  *
  * Return: If the value is not present or the array is NULL, -1.
- * 	   Otherwise, the first index where the value is located.
+ * Otherwise, the first index where the value is located.
  */
 
 int interpolation_search(int *array, size_t size, int value)
